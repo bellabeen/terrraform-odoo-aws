05627e84d68bad2b600c64fa87c88eb1b4a5234b
bellabeen <rootsquid@gmail.com>
2024-04-05 14:56:32 +0700

    [FIX] sg multiple subnet app

--- modules/ec2/sg/main.tf
+++ modules/ec2/sg/main.tf
@@ -11,22 +11,6 @@ resource "aws_security_group" "db_security_group" {
     protocol    = var.aws_local_protocol_tcp
   }
 
-  ingress {
-    cidr_blocks = var.aws_local_cidr_blocks
-    description = var.aws_local_description
-    from_port   = var.aws_local_from_db_port
-    to_port     = var.aws_local_to_db_port
-    protocol    = var.aws_local_protocol_tcp
-  }
-
-  # ingress {
-  #   cidr_blocks = var.aws_local_cidr_blocks
-  #   description = var.aws_local_description
-  #   from_port   = var.aws_local_from_db_port
-  #   to_port     = var.aws_local_to_db_port
-  #   protocol    = var.aws_local_protocol_tcp
-  # }
-
   dynamic "ingress" {
       for_each = var.subnet_ids
       content {
@@ -34,7 +18,8 @@ resource "aws_security_group" "db_security_group" {
         from_port   = var.aws_local_from_db_port
         to_port     = var.aws_local_to_db_port
         protocol    = var.aws_local_protocol_tcp
-        description = "Allow traffic from ${ingress.key} to database"
+        description = "Allow traffic from EC2 Private Subnet to database"
+        # description = "Allow traffic from ${data.aws_subnet.subnets[ingress.value].tags.Name} to database"
       }
   }
 
@@ -51,11 +36,10 @@ resource "aws_security_group" "db_security_group" {
 }
 
 data "aws_subnet" "subnets" {
-  for_each = toset(keys(var.subnet_tags))  # Use keys of the subnet tags map
+  for_each = toset(var.subnet_ids)
   vpc_id   = var.vpc_id
-  tags     = { Name = each.value }  # Use the tag value as the filter
+  tags     = { Name = "Private Subnet App A" }  # Default name to avoid errors, update accordingly
 }
-
 
 resource "aws_security_group" "alb_security_group" {
   name        = var.sg_alb_name
@@ -78,12 +62,36 @@ resource "aws_security_group" "alb_security_group" {
     protocol    = "tcp"
   }
 
-  ingress {
-    cidr_blocks = var.aws_local_cidr_blocks
-    description = var.aws_local_to_http_description
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+  # dynamic "ingress" {
+  #     for_each = var.subnet_ids
+  #     content {
+  #       cidr_blocks = [ingress.value]
+  #       from_port   = var.aws_local_from_https_port
+  #       to_port     = var.aws_local_to_https_port
+  #       protocol    = "tcp"
+  #       description = "Allow traffic from EC2 Private Subnet to database"
+  #       # description = "Allow traffic from ${data.aws_subnet.subnets[ingress.value].tags.Name} to database"
+  #     }
+  # }
+
+  # ingress {
+  #   cidr_blocks = var.aws_local_cidr_blocks
+  #   description = var.aws_local_to_http_description
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  # }
+
+   dynamic "ingress" {
+      for_each = var.subnet_ids
+      content {
+        cidr_blocks = [ingress.value]
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        description = "All Private EC2 Subnet access ALB from http"
+        # description = "Allow traffic from ${data.aws_subnet.subnets[ingress.value].tags.Name} to database"
+      }
   }
 
   tags = var.sg_alb_tags
@@ -102,20 +110,42 @@ resource "aws_security_group" "app_security_group" {
     protocol    = "-1"
   }
 
-  ingress {
-    cidr_blocks = var.aws_local_cidr_blocks
-    description = var.aws_local_description
-    from_port   = var.aws_local_from_xmlrpc_port
-    to_port     = var.aws_local_to_xmlrpc_port
-    protocol    = var.aws_local_protocol_tcp
+  # ingress {
+  #   cidr_blocks = var.aws_local_cidr_blocks
+  #   description = var.aws_local_description
+  #   from_port   = var.aws_local_from_xmlrpc_port
+  #   to_port     = var.aws_local_to_xmlrpc_port
+  #   protocol    = var.aws_local_protocol_tcp
+  # }
+
+  dynamic "ingress" {
+      for_each = var.subnet_ids
+      content {
+        cidr_blocks = [ingress.value]
+        from_port   = var.aws_local_from_xmlrpc_port
+        to_port     = var.aws_local_to_xmlrpc_port
+        protocol    = var.aws_local_protocol_tcp
+        description = "All Private EC2 Subnet access from xmlrpc port"
+      }
   }
 
-    ingress {
-    cidr_blocks = var.aws_local_cidr_blocks
-    description = var.aws_local_description
-    from_port   = var.aws_local_from_port_ssh
-    to_port     = var.aws_local_to_port_ssh
-    protocol    = var.aws_local_protocol_tcp
+  # ingress {
+  #   cidr_blocks = var.aws_local_cidr_blocks
+  #   description = var.aws_local_description
+  #   from_port   = var.aws_local_from_port_ssh
+  #   to_port     = var.aws_local_to_port_ssh
+  #   protocol    = var.aws_local_protocol_tcp
+  # }
+
+    dynamic "ingress" {
+      for_each = var.subnet_ids
+      content {
+        cidr_blocks = [ingress.value]
+        from_port   = var.aws_local_from_port_ssh
+        to_port     = var.aws_local_to_port_ssh
+        protocol    = var.aws_local_protocol_tcp
+        description = "All Private EC2 Subnet access ALB from ssh port"
+      }
   }
 
     ingress {
@@ -142,12 +172,23 @@ resource "aws_security_group" "efs_security_group" {
     protocol    = "-1"
   }
 
-  ingress {
-    cidr_blocks = var.aws_local_cidr_blocks
-    description = "from EC2 Subnet Avaibility Zone"
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "tcp"
+  # ingress {
+  #   cidr_blocks = var.aws_local_cidr_blocks
+  #   description = "from EC2 Subnet Avaibility Zone"
+  #   from_port   = 2049
+  #   to_port     = 2049
+  #   protocol    = "tcp"
+  # }
+
+    dynamic "ingress" {
+      for_each = var.subnet_ids
+      content {
+        cidr_blocks = [ingress.value]
+        from_port   = 2049
+        to_port     = 2049
+        protocol    = var.aws_local_protocol_tcp
+        description = "From Private EC2 Subnet Avaibility Zone"
+      }
   }
 
 # TODO: reference another Security Group ID
@@ -158,6 +199,5 @@ resource "aws_security_group" "efs_security_group" {
     protocol    = "tcp"
     security_groups = ["${aws_security_group.app_security_group.id}"]
   }
-
   tags = var.sg_efs_tags
 }
